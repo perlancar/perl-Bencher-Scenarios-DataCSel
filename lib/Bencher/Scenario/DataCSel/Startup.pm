70c76ca687f4ca68014b7c9e4214524188cb8fb9
perlancar (@pc-office) <perlancar@gmail.com>
2016-03-19 00:14:18 +0700

    UpdateS

--- lib/Bencher/Scenario/DataCSel/Startup.pm
+++ lib/Bencher/Scenario/DataCSel/Startup.pm
@@ -3,14 +3,12 @@ package Bencher::Scenario::DataCSel::Startup;
 # DATE
 # VERSION
 
-#use FindBin '$Bin';
-# XXX replace $^X with perl path provided by bencher
-
 our $scenario = {
     summary => 'Benchmark startup overhead of loading Data::CSel and parsing expressions',
     participants => [
-        { name => 'perl',              cmdline => [$^X, "-I$Bin/..lib", "-e1"] },
-        { name => 'load_csel_parser',  cmdline => [$^X, "-I$Bin/..lib", "-MData::CSel::Parser", "-e", 1] },
+        { name => 'perl',            perl_cmdline => ["-e1"] },
+        { name => 'load_csel',       perl_cmdline => ["-MData::CSel", "-e1"] },
+        { name => 'load_csel_parse', perl_cmdline => ["-MData::CSel=parse_csel", "-e", "parse_csel(q(E F))"] },
     ],
 };
 
