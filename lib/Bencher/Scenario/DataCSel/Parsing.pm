f6eaf4a1561a59e39b88691ddf4159098fd2034c
perlancar (@pc-office) <perlancar@gmail.com>
2016-03-24 15:18:03 +0700

    Add scenario: DataCSel::Parsing

---
+++ lib/Bencher/Scenario/DataCSel/Parsing.pm
@@ -0,0 +1,21 @@
+package Bencher::Scenario::DataCSel::Parsing;
+
+# DATE
+# VERSION
+
+our $scenario = {
+    summary => 'Benchmark parsing speed',
+    participants => [
+        { fcall_template => 'Data::CSel::parse_csel(<expr>)' },
+    ],
+    datasets => [
+        {args=>{expr=>'*'}},
+        {args=>{expr=>'T'}},
+        {args=>{expr=>'T T2 T3 T4 T5'}},
+        {args=>{expr=>'T ~ T ~ T ~ T ~ T'}},
+        {args=>{expr=>':has(T[length > 1])'}},
+    ],
+};
+
+1;
+# ABSTRACT:
